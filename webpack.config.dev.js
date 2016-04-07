b9a6dc49e68b1667756f6a0c449ecfcbe2fd10e8
AND <janaipakos@gmail.com>
2016-04-06 22:46:42 -0400

    Change config for es6

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,16 +2,12 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  // or devtool: 'eval' to debug issues with compiled output:
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    // necessary for hot reloading with IE:
-    'eventsource-polyfill',
-    // listen to code updates emitted by hot middleware:
+    'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client',
-    // your code:
     './src/index'
-   ],
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
