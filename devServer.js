caac9fb11863169218671588b51158ec9ba659fd
AND <janaipakos@gmail.com>
2016-04-07 23:39:12 -0400

    Production server

--- devServer.js
+++ devServer.js
@@ -2,28 +2,45 @@ var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config.dev');
+var webpackMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
 
+var isDevelopment = (process.env.NODE_ENV !== 'production');
+var port = isDevelopment ? 3000 : process.env.PORT;
 var app = express();
-var compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
 
-app.use(require('webpack-hot-middleware')(compiler));
+if (isDevelopment) {
+  var compiler = webpack(config);
+  var middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
 
-app.use(express.static('public'));
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.get('*', function response(req, res) {
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+    res.end();
+  });
+} else {
+  app.use(express.static(__dirname + '/dist'));
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(3000, 'localhost', function(err) {
+app.listen(port, '0.0.0.0', function onStart(err) {
   if (err) {
     console.log(err);
-    return;
   }
-
-  console.log('Listening at http://localhost:3000');
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
